a6d4cf3cb140212bb9e370f591fad37e28e34bb2
Aditi <asaki444@gmail.com>
2019-11-15 15:00:10 -0500

    added maven project

---
+++ recipe-cart/src/test/java/com/cart/recipe/cart/RecipeCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart.recipe.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
