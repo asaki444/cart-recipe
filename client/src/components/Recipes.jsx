91a11c6f9bc0ef2f980b1ccb868ba13b861bb669
Aditi <asaki444@gmail.com>
2019-11-19 12:42:53 -0500

    added recipes container

---
+++ client/src/components/Recipes.jsx
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class RecipesContainer extends Component {
+    render() {
+        return (
+            <div className="recipes-container">
+                Lists
+            </div>
+        )
+    }
+}
+
+export default RecipesContainer;
