6c1f95c51129ad6578a7fd8d4e7b6fe6a2c9056b
Aditi <asaki444@gmail.com>
2020-01-14 02:06:31 -0500

    added serializer gem

--- recipe-app/app/controllers/recipes_controller.rb
+++ recipe-app/app/controllers/recipes_controller.rb
@@ -46,6 +46,6 @@ class RecipesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def recipe_params
-      params.require(:recipe).permit(:title, :servings, :origin)
+      params.require(:recipe).permit(:title, :servings, :origin, :instruction_id)
     end
 end
