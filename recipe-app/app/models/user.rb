9046b438c29e69392e1644487666727f2b2aafd0
Aditi <asaki444@gmail.com>
2020-01-17 13:05:25 -0500

    added rspec to ruby api

--- recipe-app/app/models/user.rb
+++ recipe-app/app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_many :recipes
-  validates_presence_of :email, presence: true, uniqueness: true
 end
---
+++ recipe-app/spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'before sign in' do 
+    it 'must have email' do
+    expect {User.create.email!}.to raise_error(ActiveRecord::RecordInvalid) 
+   end
+  pending "add some examples to (or delete) #{__FILE__}"
+end
