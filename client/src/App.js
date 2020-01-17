b5ea31269b10751967a4d4d3434110c63119723d
Aditi <asaki444@gmail.com>
2020-01-16 22:19:24 -0500

    added routes to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
 import RecipesContainer  from  './components/recipes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+
+     <Switch>
+        <Route path="/recipes">
         <RecipesContainer />
-    </div>
+        </Route>
+        <Route path="/">
+          
+        </Route>
+      </Switch>
+    </Router>
+  
   );
 }
 
