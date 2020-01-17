ff0b741b89a0bbab5fe51f6910081d0e85389bc1
Aditi <asaki444@gmail.com>
2020-01-16 20:52:29 -0500

    added inputs to form

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -4,7 +4,25 @@ function RecipeForm (props){
     const ingredients = props;
     console.log(ingredients);
     return(
-      {}
+        <form>
+        <label>
+   Title:
+        <input type="text" name="title" />
+        </label>
+    <label>
+        Amount:
+        <input type="integer" name="amount"/>
+    </label>
+    <label>
+        Ingredient:
+        <input type="text" name="ingredients"/>
+    </label>
+    <label>
+        Instructions
+        <input type="text" name="instructions"/>
+    </label>
+     <input type="submit" value="Submit" />
+</form>
     )
 }
 
