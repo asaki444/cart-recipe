ceba6ff6b614d18ae3c433187dbd44e7388435a4
Aditi <asaki444@gmail.com>
2020-01-16 20:40:56 -0500

    created recipe form

---
+++ client/src/components/form.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+ 
+function RecipeForm (props){
+    const ingredients = props;
+    console.log(ingredients);
+    return(
+      {}
+    )
+}
+
+export default RecipeForm;
