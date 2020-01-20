53653be656969da7e4fc1f321fd1fc9f7bcd76be
Aditi <asaki444@gmail.com>
2020-01-19 19:57:47 -0500

    add error message for missing password

--- recipe-app/spec/models/user_spec.rb
+++ recipe-app/spec/models/user_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe User, type: :model do
     it 'must have email' do
     expect {User.create.email!}.to raise_error(ActiveRecord::RecordInvalid) 
    end
+    it 'must have password' do
+      expect {User.create.password}. to raise_error(ActiveRecord::RecordInvalid)
+  
+    end
   pending "add some examples to (or delete) #{__FILE__}"
 end
