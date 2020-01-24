dd88bf5e0e8f0adb502533fcfb6616acade413ed
Aditi <asaki444@gmail.com>
2020-01-23 23:57:26 -0500

    added secure password to user model, and password_digest column

--- recipe-app/db/schema.rb
+++ recipe-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117180156) do
+ActiveRecord::Schema.define(version: 20200124045600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20200117180156) do
     t.string   "password_confirmation"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.string   "password_digest"
   end
 
 end
