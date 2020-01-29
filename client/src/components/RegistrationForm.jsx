8a34d5bb3322477aeefd100d605e2a65f0d470ad
Aditi <asaki444@gmail.com>
2020-01-28 21:09:12 -0500

    added validate password function to registration component

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,99 +1,96 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class extends Component {
-    constructor(props){
-       super(props);
+	constructor (props) {
+		super(props);
 
-       this.state = {
-           email: "",
-           password: "",
-           username: "",
-           password_confirmation : "",
-           registrationErrors: ""
-       }
-    }
+		this.state = {
+			email                 : '',
+			password              : '',
+			username              : '',
+			password_confirmation : '',
+			registrationErrors    : ''
+		};
+	}
 
-     handleSubmit = (event)=>{
-        event.preventDefault(); 
-        const {
-            email,
-            password,
-            password_confirmation,
-            username
-        } = this.state;
+	handleSubmit = (event) => {
+		event.preventDefault();
+		const { email, password, password_confirmation, username } = this.state;
 
-        axios.post("http://localhost:3001/api/v1/registrations",
-        {
-            user:{
-                email_address: email,
-                user_name: username,
-                password: password,
-                password_confirmation: password_confirmation
-            }
-        
-        }, 
-        {
-         withCredentials: true
-        }).then(
-             response => console.log(`response: ${response}`)
-        ).catch( error => {
-            console.log("registration  error", error )
-        }
-        )
-    
+		axios
+			.post(
+				'http://localhost:3001/api/v1/registrations',
+				{
+					user : {
+						email_address         : email,
+						user_name             : username,
+						password              : password,
+						password_confirmation : password_confirmation
+					}
+				},
+				{
+					withCredentials : true
+				}
+			)
+			.then((response) => console.log(`response: ${response}`))
+			.catch((error) => {
+				console.log('registration  error', error);
+			});
+	};
 
+	validatePassword = () => {
+		const { password, password_confirmation } = this.state;
+		return password_confirmation === password;
+	};
+	handleChange = (event) => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
 
-     }
+	render () {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="email"
+						name="email"
+						placeholder="email"
+						value={this.state.email}
+						onChange={this.handleChange}
+						required
+					/>
+					<input
+						type="username"
+						name="username"
+						placeholder="Username"
+						value={this.state.username}
+						onChange={this.handleChange}
+						required
+					/>
 
-     handleChange = (event) => {
-        event.preventDefault();
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-     }
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={this.state.password}
+						onChange={this.handleChange}
+						required
+					/>
 
-    render(){
-        return (
-            <div>
-              <form onSubmit={this.handleSubmit}>
-               <input type="email" name="email" placeholder="email"
-               value={this.state.email}
-               onChange={this.handleChange}
-               required
-               />
-           <input
-               type="username"
-               name="username"
-               placeholder="Username"
-               value={this.state.username}
-               onChange={this.handleChange}
-               required
-               />
-
-               <input
-               type="password"
-               name="password"
-               placeholder="Password"
-               value={this.state.password}
-               onChange={this.handleChange}
-               required
-               />
-
-               <input
-               type="password"
-               name="password_confirmation"
-               placeholder="Password confirmation" 
-               value={this.state.password_confirmation}
-               onChange={this.handleChange}
-               required
-               />
-               <button type="submit">
-                   Registration
-               </button>
-              </form>
-
-            </div>
-        )
-    }
+					<input
+						type="password"
+						name="password_confirmation"
+						placeholder="Password confirmation"
+						value={this.state.password_confirmation}
+						onChange={this.handleChange}
+						required
+					/>
+					<button type="submit">Registration</button>
+				</form>
+			</div>
+		);
+	}
 }
